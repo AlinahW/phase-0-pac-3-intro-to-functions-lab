c1ddb4a7f7efd00c8894a4fddfe80403a1f39da9
ALinah <Williamsalinah@gmail.com>
2022-10-31 08:44:30 -0700

    commit tree 75337c9e2b07ec8771058cc1876f3c12f5ea19e2

---
+++ index.js
@@ -0,0 +1,49 @@
+function shout(string) {
+    return string;
+}
+"Hello!" .toUpperCase();
+
+function shout(string){
+return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string;
+}
+"Hello!" .toLowerCase();
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log("Hello!");
+}
+"Hello!" .toUpperCase();
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log("Hello!");
+}
+"Hello!" .toLowerCase();
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate() {
+    if (.toLowerCase()) {
+        "I can't hear you!"
+    }
+    if (.toUpperCase()) {
+        "YES INDEED!"
+    }
+    if ("Let's have dinner together!")
+        "I would love to!"
+}
+console.log("I can\'t hear you!")
+console.log("YES INDEED!")
+console.log("I would love to!")
